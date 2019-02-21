1951f8273955e5148d2796fa97addb3c99ad0f64
Chaofan Zhang <chaofan.zhang@ef.com>
2019-02-21 17:00:30 +0800

    Test Property Binding.

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,24 +1,42 @@
+import { By } from '@angular/platform-browser';
 import { VoterComponent } from './voter.component';
-import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 
 describe('VoterComponent', () => {
   let fixture: ComponentFixture<VoterComponent>;
   let component: VoterComponent;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        VoterComponent
-      ]
-    });
+      declarations: [VoterComponent]
+    }).compileComponents;
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(VoterComponent);
     component = fixture.componentInstance;
-
-    // fixture.detectChanges();
+    fixture.detectChanges();
   });
 
   it('Should Create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('totalVotes should be rendered correctly', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.vote-count'));
+    let ne: HTMLElement = de.nativeElement;
+    expect(ne.innerText).toContain('21');
+  });
+
+  it('should have highlighted class when myVote is 1', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    expect(de.classes['highlighted']).toBeTruthy();
+  })
 });
